bed7be2291fe3f527c574df891a50104fb2c5432
harpreet1o <harpreetx01@outlook.com>
2023-12-01 20:00:29 -0800

    dd

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -322,7 +322,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       document.title = `Movie | ${title}`;
 
       return function () {
-        document.title = "usePopcorn";
+        document.title = "Search Movies";
         // console.log(`Clean up effect for movie ${title}`);
       };
     },
