9fbb35a2a0a1428a43f2bee1d477cfbb58233611
harpreet1o <harpreetx01@outlook.com>
2023-12-01 20:06:27 -0800

    dd

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -28,7 +28,6 @@ export default function SignUp() {
 
       console.log(response);
       if (response.data.status === "ok") {
-        alert("signup done");
         navigate(`/`);
       }
     } catch (error) {
